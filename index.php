480a9c985a6b2e0f1a350b9c8f46873a6c10f7f6
santi <snt.mv13@gmail.com>
2017-07-13 23:26:54 +0200

    First structure done

---
+++ index.php
@@ -0,0 +1,142 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="es/ES">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+    <title>Aprender a progrmamar html, css, javascript y angular</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+  <hader>
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">VivitCode</a>
+          </div>
+          <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <section class="main">
+          <div class="container">
+              <div class="col-lg-7">
+                 <h1>Aprender a programar és facil... <br>pero ponle ganas!</h1>
+                 <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+              </div>
+              <div class="col-lg-offset-1 col-lg-4 center">
+                 <img class="header-img" src="img/codigo-anaranjado.jpg">
+              </div>
+          </div>
+      </section>
+  </header>
+  <section>
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-3 cards">
+                  <div class="lenguaje">
+                      <h2>HTML</h2>
+                      <img class="logos" src="img/html.png">
+                  </div>
+                  <p>Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+              </div>
+              <div class="col-lg-3 cards">
+                  <div class="lenguaje">
+                      <h2>CSS</h2>
+                      <img class="logos" src="img/css3.png">
+                  </div>
+                  <p>Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+              </div>
+              <div class="col-lg-3 cards">
+                  <div class="lenguaje">
+                      <h2>BOOTSTRAP</h2>
+                      <img class="logos" src="img/bootstrap.png">
+                  </div>
+                  <p>Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-lg-3 cards">
+                  <div class="lenguaje">
+                      <h2>JAVASCRIPT</h2>
+                      <img class="logos" src="img/javascript.png">
+                  </div>
+                  <p>Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+              </div>
+              <div class="col-lg-3 cards">
+                  <div class="lenguaje">
+                     <h2>JQUERY</h2>
+                     <img class="logos" src="img/jquery.png">
+                  </div>
+                  <p>Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+              </div>
+            <div class="col-lg-3 cards">
+                  <div class="lenguaje">
+                      <h2>ANGULAR</h2>
+                      <img class="logos" src="img/angular.png">
+                  </div>
+                  <p>Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+            </div>
+          </div>
+      </div>
+  </section>
+  <section class="next">
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-offset-1 col-lg-10 centered">
+                 <h1>¿Por que todos tendríamos que aprender a programar?</h1>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-lg-offset-2 col-lg-8 centered">
+                  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+          </div>
+      </div>
+  </section>
+  <footer>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-offset-1 col-lg-2">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          </p>
+        </div>
+        <div class="col-lg-offset-6  col-lg-2">
+        <p>
+            LLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
